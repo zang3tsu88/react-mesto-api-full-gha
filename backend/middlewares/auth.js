977d8700b27c4e53f115a955118fd836a6f522f5
zang3tsu88 <maximenya.a@gmail.com>
2023-06-30 22:09:00 +0300

    debug: testing env variables

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
+const { SECRET_KEY } = require('../utils/constants');
+
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -16,7 +18,8 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    // payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
     throw new Unauthorized('Authorization required.');
   }
--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,4 +1,4 @@
-const SECRET_KEY = 'mySecretKey';
+const SECRET_KEY = 'dev-secret';
 const URL_REGEX = /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
 
 module.exports = {
